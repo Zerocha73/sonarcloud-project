c701dd43168b1be847b12c197e6f90c227c5a6a4
Zerocha73 <129547909+Zerocha73@users.noreply.github.com>
2023-03-31 21:34:49 +0200

    Delete examples directory

--- examples/MIMXRT1xxx/MIMXRT1021/baremetal/uart-timing-demo/fwver.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FWVER_H__
-#define __FWVER_H__
-
-#include <stdint.h>
-
-extern const uint32_t FIRMWARE_VERSION;
-extern const uint32_t BUILD_TIME;
-
-#endif
--- examples/MIMXRT1xxx/MIMXRT1021/baremetal/uart-timing-demo/main.h
+++
@@ -1,6 +0,0 @@
-#include "mcuid.h"
-#include "system.h"
-#include "delay.h"
-#include "debug.h"
-
-#define debugMAIN(type, ...)			printDEBUG(type, "SYS", __VA_ARGS__)
